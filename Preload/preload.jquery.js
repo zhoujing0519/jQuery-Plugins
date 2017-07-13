09858e9f9a4679a61c75ba43ce6f55704312aead
zhoujing0519 <403502303@qq.com>
2017-07-13 10:04:31 +0800

    ordered preload

--- Preload/preload.jquery.js
+++ Preload/preload.jquery.js
@@ -5,14 +5,43 @@
 		this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
 		this.opts = $.extend({}, Preload.DEFAULT, opts);
 
-		this._unordered();
+		this.opts.order === 'ordered' ? this._ordered() : this._unordered();
 	}
 
 	// 默认配置
 	Preload.DEFAULT = {
+		order: 'unordered', // 默认无序加载
 		each: null, // 每张图片加载完成之后执行的操作
 		all: null // 所有图片加载完成之后执行的操作
 	}
+
+	// 有序加载
+	Preload.prototype._ordered = function(){
+		var opts = this.opts,
+			imgs = this.imgs,
+			len = imgs.length,
+			count = 0;
+
+		load();
+
+		function load(){
+			var oImg = new Image();
+
+			$(oImg).on('load error', function(){
+				opts.each && opts.each(count);
+
+				if(count >= len){
+					opts.all && opts.all();
+				}else{
+					load();
+				}
+
+				count++;
+			});
+
+			oImg.src = imgs[count];
+		}
+	};
 
 	// 无序加载
 	Preload.prototype._unordered = function(){
